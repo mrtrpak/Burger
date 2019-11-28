ac282df6dc69aded8df965b6eb72e08809081085
Michael Trpak <mrtrpak3@gmail.com>
2019-11-28 15:59:04 -0500

    took out excess code in server.js

--- server.js
+++ server.js
@@ -13,13 +13,6 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-// const connection = mysql.createConnection({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "rootroot",
-//     database: "burgers_db"
-// });
 let connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
