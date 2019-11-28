62ce29c8eab048f4650c332fd80d40c6d9317ce5
Michael Trpak <mrtrpak3@gmail.com>
2019-11-28 15:35:45 -0500

    setting up with heroku

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.set("view engine", "handlebars");
 //     password: "rootroot",
 //     database: "burgers_db"
 // });
-const connection;
+let connection;
 if (process.env.JAWSDB_URL) {
     connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
